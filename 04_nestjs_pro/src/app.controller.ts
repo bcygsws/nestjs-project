39442426ffa7d9b8c10719f4a2bc4e3122a64958
bcygsws <bcygswspring168@gmail.com>
2024-08-21 21:18:28 +0800

    :memo: Nest项目开始

--- 04_nestjs_pro/src/app.controller.ts
+++ 04_nestjs_pro/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from './app.service';
 
+// 1.路由地址拼接规则：localhost:3000/+@Controller注解中的字符串+@Get注解中的字符串
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    // 2.将AppService依赖注入到当前类中；就可以使用AppService类的方法，不需要new AppService(),也不要修改；故而声明为private readonly
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
 }
